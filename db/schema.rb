0b4a9da97b54ffcfdbc3e17a19c4912abcc72aee
Zmago Devetak <zmago_devetak@yahoo.com>
2016-06-14 09:25:46 +0000

    update views and navigation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603095338) do
+ActiveRecord::Schema.define(version: 20160614084611) do
 
   create_table "stocks", force: true do |t|
     t.string   "ticker"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20160603095338) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
